a7cce9e72f038bf6015dc1ced29dc09af94da195
Venterek <jan.wielgus12@op.pl>
2017-05-14 16:18:57 +0200

    Zmany w algorytmie odczytu z barometru

--- HQbarometrTest/HQbarometrTest.ino
+++ HQbarometrTest/HQbarometrTest.ino
@@ -2,7 +2,7 @@
     #include <MS5611.h>
     #include "FiltrKalmana.h"
      
-    MS5611 ms5611;
+    MS5611 baro;
     FiltrKalmanaClass filtrk;
      
     double referencePressure;
@@ -20,7 +20,7 @@
       // Standard: MS5611_STANDARD
       // Low power: MS5611_LOW_POWER
       // Ultra low power: MS5611_ULTRA_LOW_POWER
-      while(!ms5611.begin(MS5611_HIGH_RES))
+      while(!baro.begin(MS5611_HIGH_RES))
       {
         Serial.println("Nie mozna znalezc czujnika MS5611, sprawdz polaczenie!");
         delay(500);
@@ -28,7 +28,7 @@
 
      delay(300);
       // Pobieramy poczatkowe cisnienie do okreslenia wysokosci
-      referencePressure = ms5611.readPressure();
+      referencePressure = baro.readPressure();
      
       // Sprawdzamy ustawienia
       checkSettings();
@@ -37,24 +37,19 @@
     void checkSettings()
     {
       Serial.print("Oversampling: ");
-      Serial.println(ms5611.getOversampling());
+      Serial.println(baro.getOversampling());
     }
      
     void loop()
     {
       // Odczyt przekonwertowanych wartosci
-      long realPressure = ms5611.readPressure();
+      long realPressure = baro.readPressure(true);
      
       // Obliczanie wysokosci
-      float absoluteAltitude = ms5611.getAltitude(realPressure);
-      float relativeAltitude = ms5611.getAltitude(realPressure, referencePressure);
-      float pofiltrze = filtrk.update(absoluteAltitude, 0);
-     
-      Serial.print(" absoluteAltitude = ");
-      Serial.print(absoluteAltitude);
-      Serial.print(" m\t relativeAltitude = ");
-      Serial.print(relativeAltitude);    
-      Serial.print(" m\t");
-      Serial.println(pofiltrze*10);
+      float absoluteAltitude = baro.getAltitude(realPressure);
+      uint16_t pofiltrze = (uint16_t)(filtrk.update(absoluteAltitude, 0)*100);
+      
+      //Serial.print(" m\t");
+      Serial.println(pofiltrze);
     }
 
---
+++ HQbarometrTest/Release/HQbarometrTest.cpp.d
@@ -0,0 +1,19 @@
+C:\Users\Patmaker\AppData\Local\Temp\VMBuilds\HQbarometrTest\nano_atmega328\Release\HQbarometrTest.cpp.o: \
+ C:\Users\Patmaker\AppData\Local\Temp\VMBuilds\HQbarometrTest\nano_atmega328\Release\HQbarometrTest.cpp \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\Patmaker\Documents\Arduino\libraries\Arduino-MS5611-master/MS5611.h \
+ C:\Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\Patmaker\AppData\Local\Temp\VMBuilds\HQbarometrTest\nano_atmega328\Release\FiltrKalmana.h
